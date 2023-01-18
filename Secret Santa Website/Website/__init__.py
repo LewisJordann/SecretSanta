bc5d51a84c3dbe2078a62438ac9b317e821e0e65
Jordan Lewis <jsl042@office.latech.edu>
2023-01-18 16:52:45 -0600

    Added Home Page, Fixed redirects

--- Secret Santa Website/Website/__init__.py
+++ Secret Santa Website/Website/__init__.py
@@ -3,10 +3,13 @@ from flask import Flask, url_for
 def create_app():
     # initalize Flask
     app = Flask(__name__, template_folder="template", static_folder="static")
+
+    # remove secret key before pushing
     app.config['SECRET_KEY'] = ''
 
     from .views import views
     from .auth import auth
+
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
